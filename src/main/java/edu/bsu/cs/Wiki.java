5e29be6e687bf9b132a6c27db8245b7725ff02c8
Buddy922 <wyvern922@gmail.com>
2025-02-06 08:08:17 -0800

    Add Wiki Connection

--- src/main/java/edu/bsu/cs/Wiki.java
+++ src/main/java/edu/bsu/cs/Wiki.java
@@ -1,5 +1,14 @@
 package edu.bsu.cs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
 public class Wiki {
     public String grabData(){
         Menu menu = new Menu();
@@ -8,5 +17,29 @@ public class Wiki {
     }
     public boolean scanData(){
         return false;
+    }
+    public static void main(String[] args) throws IOException {
+        URLConnection connection = connectToWikipedia();
+        String jsonData = readJsonAsStringFrom(connection);
+        printRawJson(jsonData);
+    }
+    private static URLConnection connectToWikipedia() throws IOException {
+        String encodedUrlString = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" +
+                URLEncoder.encode("Zappa", Charset.defaultCharset()) +
+                "&rvprop=timestamp|user&rvlimit=4&redirects";
+        URL url = new URL(encodedUrlString);
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent",
+                "CS222FirstProject/0.1 (reed.stwalley@bsu.edu)");
+        connection.connect();
+        return connection;
+    }
+
+    private static String readJsonAsStringFrom(URLConnection connection) throws IOException {
+        return new String(connection.getInputStream().readAllBytes(), Charset.defaultCharset());
+    }
+
+    private static void printRawJson(String jsonData) {
+        System.out.println(jsonData);
     }
 }
