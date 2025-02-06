be61d4bba9b872e0f7002d3f2b69566835e5e2a7
Buddy922 <wyvern922@gmail.com>
2025-02-06 08:48:56 -0800

    Add Demo Tests

--- src/test/java/edu/bsu/cs/Test.java
+++ src/test/java/edu/bsu/cs/Test.java
@@ -1,12 +1,25 @@
 package edu.bsu.cs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+import org.junit.jupiter.api.Assertions;
+
 public class Test {
-    /*@Test
-    public void testResults(){
-        Menu menu = new Menu();
-        final String input = menu.getInput();
-        boolean checkInput =
+
+    @org.junit.jupiter.api.Test
+    public void testAccessToJsonFile() throws IOException {
+        String jsonData = readSampleFileAsString();
+        Assertions.assertNotNull(jsonData);
     }
 
-     */
+    private String readSampleFileAsString() throws NullPointerException, IOException {
+        try (InputStream sampleFile = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("sample.json")) {
+            return new String(Objects.requireNonNull(sampleFile).readAllBytes(), Charset.defaultCharset());
+        }
+    }
+
 }
