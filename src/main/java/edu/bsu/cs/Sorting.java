7fdcfeba4ba2cea6101db3783abc9bd2428f7b9d
Joey2 <Joey26113@gmail.com>
2025-02-06 12:17:06 -0500

    Create framework

--- src/main/java/edu/bsu/cs/Sorting.java
+++ src/main/java/edu/bsu/cs/Sorting.java
@@ -1,7 +1,15 @@
 package edu.bsu.cs;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 public class Sorting {
-    public String reverseChronological(){
+    public String reverseChronological(URLConnection connection) throws IOException {
+        Wiki wiki = new Wiki();
+        String data = wiki.readJsonAsStringFrom(connection);
+        System.out.println(data);
         return "";
     }
     public String listChanges(){
