ebe3bc74809aacc940872a0437e022fc0f2f2fb4
Joey2 <Joey26113@gmail.com>
2025-02-06 11:52:00 -0500

    Create framework

--- src/main/java/edu/bsu/cs/Menu.java
+++ src/main/java/edu/bsu/cs/Menu.java
@@ -1,14 +1,25 @@
 package edu.bsu.cs;
 
+import java.net.URLConnection;
 import java.util.Scanner;
 
 public class Menu {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        String searchTerm = menu.getInput();
+    }
     public String getInput(){
         Scanner scnr = new Scanner(System.in);
         System.out.println("Enter search request: ");
         String request = scnr.nextLine();
         return request;
     }
+    /*public String displayOutput(){
+        Wiki wiki = new Wiki();
+
+    }
+
+     */
 
 }
 
