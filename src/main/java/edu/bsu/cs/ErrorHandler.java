241d835f8dc7ee22bc24d4eb942716af1d951949
Buddy922 <wyvern922@gmail.com>
2025-02-11 14:11:48 -0800

    Add Error Handling for Connection, Empty request, and Missing article

--- src/main/java/edu/bsu/cs/ErrorHandler.java
+++ src/main/java/edu/bsu/cs/ErrorHandler.java
@@ -1,10 +1,28 @@
 package edu.bsu.cs;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+
+import java.net.URL;
+
 public class ErrorHandler {
-    public String handleErrors(){
-        return "";
+    public void checkEmptyRequest(String request){
+        if(request.isEmpty()){
+            System.err.print("You did not provide an article.\n");
+        }
     }
-    public String errorResponse(){
-        return "";
+    public void checkConnection(URL url){
+        try {
+            url.openConnection().connect();
+        }
+        catch (Exception NetworkError){
+            System.err.print("There was a network error; could not connect to the article");
+        }
+    }
+    public void checkIfMissingArticle(String jsonString){
+        JSONArray checkForMissing = JsonPath.read(jsonString, "$..missing");
+        if(!checkForMissing.isEmpty()){
+            System.err.print("The article you were looking for doesn't exist.");
+        }
     }
 }
